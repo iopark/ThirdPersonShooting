5d22fcc2ddc3cfccf59285044a11d241d90db427
here_io <onedupark@outlook.com>
2023-05-31 13:33:00 +0900

    feat: TPS 카메라 메뉴얼 생성 너무 힘듭니다 ㅠㅜ

--- Assets/Scripts/HW0530/HW_TPSCameraController.cs
+++ Assets/Scripts/HW0530/HW_TPSCameraController.cs
@@ -7,42 +7,44 @@ public class HW_TPSCameraController : MonoBehaviour
 {
     // 플레이어는 카메라의 중심을 기점으로 머리를 움직여야만 한다. 
     [Header("CameraView")]
-    private GameObject player;
+    private GameObject player; // 회전축의 중심이기도 하다. 
     public Transform shoulder;
     private Camera self;
-    private Vector3 gunPoint; 
+    [SerializeField] private Vector3 gunPoint; 
 
     //BOTH FPS and TPS Camera 는 Virtual Camera 가 아닌 일반 카메라로 구현해보았습니다. 
     [Header("Pertaining to Camera Movement")]
     [SerializeField] private Vector3 lookDelta; // Mouse input value, determined by changes applied by the pointer movement 
     [SerializeField] private float xRotation; // Camera 는 x축으로도 회전이 가능하며, (상하 movement) 
     [SerializeField] private float yRotation; // 카메라는 y축으로도 회전이 가능하다 (좌우 movement) 
+    [SerializeField] private float playerRotation; 
     [Range(0f, 5f)]
     private float rotationSpeed;
-    [SerializeField] private Vector3 offSet; 
+    [SerializeField] private Vector3 offSet; // Radius 이기도 한 값이며, 
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player"); 
         transform.position = shoulder.transform.position;
-        transform.forward = shoulder.transform.forward;
+        transform.forward = player.transform.forward;
         transform.rotation = shoulder.transform.rotation;
+        offSet = shoulder.transform.position - player.transform.position;
         //transform.SetParent(shoulder.transform); 
         self = GetComponent<Camera>();
     }
     private void Start()
     {
         //offSet = shoulder.position - player.transform.position;
-        gunPoint = self.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1));
+        gunPoint = self.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1)); // 뷰프러스텀 기준 가장 최종 사각형의 중앙값을 기준으로 플레이어는 집중한다. (in some cases, fps게임에서는 z값을 0으로 잡고 조준점으로 잡는경우도 있다) 
         rotationSpeed = 3f;
     }
     private void Update()
     {
-        
+        Look();
     }
     private void LateUpdate()
     {
-        Look();
+        //transform.position = player.transform.position + offSet; 
         Rotate();
     }
     private void OnLook(InputValue value)
@@ -59,9 +61,37 @@ public class HW_TPSCameraController : MonoBehaviour
     {
         Cursor.lockState = CursorLockMode.None;
     }
+
+    private void CameraOrbit()
+    {
+        float x = player.transform.position.x + offSet.magnitude * Mathf.Cos(playerRotation * Mathf.Deg2Rad);
+        float z = player.transform.position.z + offSet.magnitude * Mathf.Sin(playerRotation * Mathf.Deg2Rad);
+        Vector3 newPosition = new Vector3(x, shoulder.transform.position.y, z);
+        // Move the object to the desired position
+        transform.forward = shoulder.transform.forward;
+        transform.position = newPosition;
+        
+    }
+
     private void HeadBob()
     {
-        player.transform.LookAt(gunPoint); 
+        //Vector3 direction = gunPoint - player.transform.position;// 플레이어와 카메라 정중앙 위치의 벡터를 구한뒤 
+        //direction.y = player.transform.position.y; // y 값은 플레이어의 값과 동일하게 하여 x축에 대한 회전은 그냥 방지한다 // 이럴땐 Bobing해드를 만들어 머리만 댕강움직이게 한다. 
+        //Quaternion rotation = Quaternion.FromToRotation(player.transform.forward, direction);
+        //// Apply the rotation to the GameObject, keeping its original rotation on other axes
+        //rotation.y = player.transform.position.y;
+        //playerRotation = rotation.eulerAngles.y;
+        //player.transform.rotation *= rotation; 
+
+        Vector3 direction = gunPoint - player.transform.position;
+        direction.y = player.transform.position.y;
+        //direction.z = player.transform.position.z;
+        Quaternion rotation = Quaternion.FromToRotation(player.transform.position, direction);
+        //Quaternion forwardRotation = Quaternion.AngleAxis(player.transform.rotation.eulerAngles.y, Vector3.up);
+        //Quaternion finalRotation = rotation * forwardRotation;
+        playerRotation = rotation.eulerAngles.y;
+        player.transform.rotation = rotation; 
+
     }
 
     private void Look()
@@ -70,6 +100,8 @@ public class HW_TPSCameraController : MonoBehaviour
         yRotation += lookDelta.x * rotationSpeed * Time.deltaTime;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0); //해당 Transform 의 로컬회전하는 값은 Degree값이다,
+        gunPoint = self.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1));
+        
     }
 
     /// <summary>
@@ -77,8 +109,7 @@ public class HW_TPSCameraController : MonoBehaviour
     /// </summary>
     private void Rotate()
     {
-        gunPoint = self.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 1));
-        gunPoint.y = player.transform.position.y;
         HeadBob();
+        CameraOrbit();
     }
 }
